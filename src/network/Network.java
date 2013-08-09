d924f12cec8238a2bd9ee201c649d3b5ede8e816
izban <izban@mail.ru>
2013-08-09 18:06:13 +1100

    lol

---
+++ src/network/Network.java
@@ -0,0 +1,15 @@
+package network;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: izban
+ * Date: 09.08.13
+ * Time: 17:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Network {
+    public void main() {
+
+    }
+}
