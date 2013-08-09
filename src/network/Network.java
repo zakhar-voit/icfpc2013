ae931178d82473e9783a5b4cb63c4558dc7d1a99
izban <izban@mail.ru>
2013-08-09 18:55:56 +1100

    lol

--- src/network/Network.java
+++ src/network/Network.java
@@ -1,5 +1,10 @@
 package network;
 
+import java.net.*;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: izban
@@ -9,7 +14,40 @@ package network;
  */
 
 public class Network {
-    public void main() {
+    public void Run() {
+        URL myUrl;
+        HttpURLConnection myConnect = null;
 
+        try {
+            String request = "request body TrainRequest";
+            myUrl = new URL("http://icfpc2013.cloudapp.net/train?auth=03666BdvADBV1jujD9NNrr9pD3NJENbYB2r41s2WvpsH1H");
+            myConnect = (HttpURLConnection)myUrl.openConnection();
+
+            /*myConnect.setRequestMethod("POST");
+            myConnect.setRequestProperty("Content-Length", "" + Integer.toString(request.getBytes().length));
+
+            myConnect.setUseCaches (false);
+            myConnect.setDoInput(true);
+            myConnect.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream(myConnect.getOutputStream());
+            wr.writeBytes(request);
+            wr.flush();
+            wr.close();      */
+
+            InputStream is = myConnect.getInputStream();
+            Scanner in = new Scanner(is);
+            while (in.hasNextLine()) {
+                System.out.println(in.nextLine());
+            }
+
+
+            in.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (myConnect != null) myConnect.disconnect();
+        }
     }
 }
