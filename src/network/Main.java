180b015f5f4b440e7d4d4d0bb14f4e2fe7e81cc3
izban <izban@mail.ru>
2013-08-10 18:43:20 +1100

    new branch

--- src/network/Main.java
+++ src/network/Main.java
@@ -1,6 +1,9 @@
 package network;
 
 import org.json.simple.JSONObject;
+import sun.awt.windows.ThemeReader;
+
+import java.awt.font.NumericShaper;
 
 /**
  * @author Ilya Zban(izban@mail.ru)
@@ -8,8 +11,15 @@ import org.json.simple.JSONObject;
 public class Main {
 
     public void run() {
-        //new Solver().getProblems();
-        new Solver().run();
+        new Solver().getProblems();
+        /*for (int i = 0; i < 5; i++) {
+            new Solver().run();
+            try {
+                Thread.sleep(25000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } */
     }
 
     public static void main(String[] args) {
